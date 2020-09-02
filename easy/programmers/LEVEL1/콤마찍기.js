fa1fe68f1da21c954029f3e359b4d201b59e3a11
unknown <superfly09@daum.net>
2020-09-02 15:26:37 +0900

    빈칸채우기

---
+++ easy/programmers/LEVEL1/콤마찍기.js
@@ -0,0 +1,23 @@
+/*
+원범이는 편의점 아르바이트가 끝난 후 정산을 하고자 합니다.
+정산을 빨리하고 집에 가고 싶은 원범이는 프로그램을 만들려고 합니다.
+
+**숫자를 입력받고 천 단위로 콤마(,)를 찍어주세요.**
+
+예를 들어, 
+**123456789**를 입력받았으면 **123,456,789**를 출력해야 합니다.
+*/
+
+const value = '123456789';
+
+let arr =value.split('').reverse();
+
+let result = arr.map((numberValue,index)=>{
+  if (index === arr.length-1) return numberValue;
+  if (index % 3 ===2 ) {
+    return numberValue = `,${numberValue}`;
+  } 
+  return numberValue;
+}).reverse().join('');
+
+console.log(result);
