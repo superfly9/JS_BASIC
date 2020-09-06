941318ea16c5070c2ecb11daaeb1c11247e52069
unknown <superfly09@daum.net>
2020-09-06 11:18:05 +0900

    nesting router연습

--- react_practice/src/App.js
+++ react_practice/src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Todo from './Todo';
+import {BrowserRouter,Link,Route} from 'react-router-dom';
+import Rooms from './Component/Rooms';
 
 function App() {
   return (
-    <div className="App">
-      <Todo />
-    </div>
+    <BrowserRouter>
+      <div className="App" style={{padding:20,border:'2px solid black'}}>
+        <Link to='/'>홈</Link>
+        <br />
+        <Link to='/photo'>사진</Link>
+        <br />
+        <Link to='/rooms'>방 소개</Link>
+        <br />
+        <Route path='/' exact component={Home} />
+        <Route path='/photo' component={Photo} />
+        <Route path='/rooms' component={Rooms} />
+        <Todo />
+      </div>
+    </BrowserRouter>
   );
 }
+
+const Home = ()=><h2>홈페이지입니다</h2>
+const Photo =()=><h2>여기서 사진을 감상하세요</h2>
+
 
 export default App;
