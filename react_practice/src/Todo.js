aec9512d34a73fb8246296d61ff857298da54f7a
unknown <superfly09@daum.net>
2020-09-06 20:12:38 +0900

    클로져 counter연습

--- react_practice/src/Todo.js
+++ react_practice/src/Todo.js
@@ -1,6 +1,7 @@
-import React, { Fragment,useState } from 'react'
+import React, {useState } from 'react'
 
 function Todo() {
+    // UI제외한 나머지 로직들을 다루는 부분
     const [todoList,setTodo] = useState([]);
     const [currentId,setCurrentId] = useState(1);
     const [descValue,setDescValue] = useState('');
@@ -24,7 +25,7 @@ function Todo() {
     }
     const handleOdd = () => setShowOdd(!showOdd);
 
-    //홀수 아이템만 보이게 하기
+    //홀수 아이템만 보이게 하기, showOdd가 true가 되었을때 해당 목록의 index를 2로 나눈 나머지를 확인
     const renderTodo = todoList.filter((_ ,index)=> (showOdd ? index%2 === 0 : true )).map((todo,index)=>(
         <li key={todo.currentId}>
             <span style={{marginRight:'1rem'}}>{todo.description}</span>
@@ -35,6 +36,7 @@ function Todo() {
     ))
     const handleChange = (e) => setDescValue(e.currentTarget.value); 
     return (
+        //JSX=>UI담당(어떤 모습으로 렌더링될지를 결정)
         <div>
             <h3>할 일 목록</h3>
             <ul>
