d25bcd378054d1a0dd23885816a1c769d23bb3c8
unknown <superfly09@daum.net>
2020-09-07 14:37:04 +0900

    김서방을찾았습니다

---
+++ easy/programmers/LEVEL1/서울에서김서방찾기.js
@@ -0,0 +1,4 @@
+function solution(seoul) {
+    console.log(`김서방은 ${seoul.indexOf('Kim')}에 있다`)
+    return `김서방은 ${seoul.indexOf('Kim')}에 있다`
+}
---
+++ react_practice/src/2장/App.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import './App.css';
+import Todo from './Todo';
+import {BrowserRouter,Link,Route, Switch} from 'react-router-dom';
+import Rooms from './Component/Rooms';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div style={{padding:20,border:'2px solid black'}}>
+        <ul style={{listStyle:'none',padding:0}}>
+          <li>
+            <Link to='/'>홈</Link>
+          </li>
+          <li>
+            <Link to='/photo'>사진</Link>
+          </li>
+          <li>
+            <Link to='/rooms'>방 소개</Link>
+          </li>
+          <li>
+            <Link to='/todo'>메모장</Link>
+          </li>
+        </ul>
+        
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/photo' component={Photo} />
+          <Route path='/rooms' component={Rooms} />
+          <Route path='/todo' component={Todo} />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+const Home = ()=><h2>홈페이지입니다</h2>
+const Photo =()=><h2>여기서 사진을 감상하세요</h2>
+
+
+export default App;
