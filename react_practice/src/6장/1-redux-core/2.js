81b9368b1675965289672b448ca7888e2212f5f5
unknown <superfly09@daum.net>
2020-10-04 13:46:16 +0900

    middleware와 reducer의 실행 순서

--- react_practice/src/6장/1-redux-core/2.js
+++ react_practice/src/6장/1-redux-core/2.js
@@ -3,14 +3,14 @@ import {createStore,applyMiddleware} from 'redux';
 
 const middleWare1 = store => next => action => {
     console.log('mid1 start');
-    const result = next(action);
+    const result = next(action); //middleware2 실행
     console.log('mid1 end');
     return result;
 }
 
 const middleWare2 = store => next => action => {
     console.log('mid2 start');
-    const result = next(action);
+    const result = next(action); //reducer실행
     console.log('mid2 end');
     return result;
 }
@@ -25,3 +25,17 @@ store.dispatch({type:'action'});
 export default function App () {
     return <div>리덕스 연습</div>
 }
+
+//리덕스 실행순서 
+//상태 초기화 위해 리듀서 실행 
+//dispatch발생하면 applyMiddleware에 전달한 middleware 먼저 실행 후 reducer실행한다
+
+/*
+결과
+my Reducer
+mid1 start
+mid2 start
+my Reducer
+mid2 end
+mid1 end
+*/
